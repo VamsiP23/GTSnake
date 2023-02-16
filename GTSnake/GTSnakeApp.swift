4982efec85cc228eaa2fca098f8bddc3db5be9d4
Maksim Tochilkin <mtoch777@gmail.com>
2023-02-16 18:07:05 -0500

    Initial Commit

---
+++ GTSnake/GTSnakeApp.swift
@@ -0,0 +1,17 @@
+//
+//  GTSnakeApp.swift
+//  GTSnake
+//
+//  Created by Maksim Tochilkin on 2/16/23.
+//
+
+import SwiftUI
+
+@main
+struct GTSnakeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
